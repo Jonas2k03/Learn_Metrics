75566648c3cdcf7b0f806ee1b594e68f2d675e41
vBaxtry <jcfernandezc@unicauca.edu.co>
2024-11-18 11:46:17 -0500

    Cambio en Asignatura (Implementacion de asociar competencia)

--- lm/src/main/java/edu/co/unicauca/learnmetrics/lm/FachadaServices/services/AsignaturaServices/AsignaturaServiceImpl.java
+++ lm/src/main/java/edu/co/unicauca/learnmetrics/lm/FachadaServices/services/AsignaturaServices/AsignaturaServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import edu.co.unicauca.learnmetrics.lm.CapaAccesoDatos.Modelos.Asignatura.AsignaturaEntity;
+import edu.co.unicauca.learnmetrics.lm.CapaAccesoDatos.Modelos.Competencia.CompetenciaEntity;
 import edu.co.unicauca.learnmetrics.lm.CapaAccesoDatos.Repositorios.AsignaturaRepository;
 import edu.co.unicauca.learnmetrics.lm.FachadaServices.DTO.AsignaturaDTO;
 
@@ -73,8 +74,17 @@ public class AsignaturaServiceImpl implements IAsignaturaService {
 
     @Override
     public AsignaturaDTO asociarCompetencia(Integer asigId, Integer compId) {
+        AsignaturaEntity asignaturaEntity = servicioAccesoBaseDatos.findById(asigId);
+        CompetenciaEntity compEntity = new CompetenciaEntity();
+        if (asignaturaEntity != null) {
+            compEntity.setCompId(compId);
+            AsignaturaEntity updatedAsignatura = servicioAccesoBaseDatos.update(asigId, asignaturaEntity);
+            if (updatedAsignatura != null) {
+                return modelMapper.map(updatedAsignatura, AsignaturaDTO.class);
+            }
+        }
         return null;
-        // Implementar lógica para asociar competencia
+        
     }
 
 }
